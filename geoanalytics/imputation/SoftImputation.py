841f41afabf3b2e5760a440d2d9afd5d6149e2a7
vanithakattumuri <vanithakattumuri32@gmail.com>
2025-07-22 23:26:05 +0900

    # updated the sphinxDocs

--- geoanalytics/imputation/SoftImputation.py
+++ geoanalytics/imputation/SoftImputation.py
@@ -129,7 +129,7 @@ class SoftImputation:
         and returns the imputed DataFrame with original coordinates.
 
         Returns:
-        --------
+
         imputedDF : pandas.DataFrame
             The DataFrame with missing values imputed.
         """
